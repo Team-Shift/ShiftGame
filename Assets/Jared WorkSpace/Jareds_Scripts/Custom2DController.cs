513fec4d15b69763c1c01d6b758d4dd285630c32
Jared Ramey <rameyjared@gmail.com>
2016-03-10 17:20:35 -0800

    2D controller is up and running and can be worked on by others. Need to add combat to it

---
+++ Assets/Jared WorkSpace/Jareds_Scripts/Custom2DController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class Custom2DController : MonoBehaviour
+{
+    public GameObject player;
+    public float turnSpeed = 180f;
+    public float speed = 6.0f;
+    public float gravity = 20.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    private Rigidbody body;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        transform.Translate(moveDirection * Time.deltaTime * speed, Space.World);
+
+    }
+
+    void DamageFallback()
+    {
+        //Push the player back
+        transform.Translate(new Vector3());
+    }
+}
