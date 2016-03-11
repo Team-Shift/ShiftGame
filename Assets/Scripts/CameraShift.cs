b63cde336ced3759a9b3332afce069a6246e3229
Wade Gushikuma <wade.gushikuma@students.aie.edu.au>
2016-03-11 13:34:16 -0800

    camera shift working with fading in and out. try2

--- Assets/Scripts/CameraShift.cs
+++ Assets/Scripts/CameraShift.cs
@@ -7,12 +7,26 @@ public class CameraShift : MonoBehaviour {
     public GameObject ortho;
     public GameObject persp;
 
-    public bool canShift;
+    // for fading in/out
+    Color s_fade;                   // to change alpha
+    SpriteRenderer sprite;          // set color
+    bool canFade;                   // used to fade in update
+    float startTime;                // for pingPonging fade to start at 0
+    public float duration = 0.5f;   // how long to fade
+
+    public bool canShift;           // if player unlocks shift ability
+
     void Start()
     {
         // changes if unlocked in game
         canShift = true;
 
+        // get spriterenderer
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        // set s_fade
+        s_fade = sprite.color;
+        canFade = false;
+        
         // get cameras if not found
         if(ortho == null)
             ortho = GameObject.FindGameObjectWithTag("MainCamera");
@@ -20,12 +34,22 @@ public class CameraShift : MonoBehaviour {
             persp = GameObject.FindGameObjectWithTag("Perspective Camera");
     }
 
-	void FixedUpdate () {
+	void Update () {
 	    if(Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift) && canShift)
         {
-            ChangeCamera();
+            // enable fading in and out and set startTime 
+            canFade = true;
+            startTime = Time.unscaledTime;
+            // change camera at half duration
+            StartCoroutine("CameraChange");
+            // disable fading after full fade duration
+            StartCoroutine("ShiftFade");
         }
-	}
+        // start fading in and out
+        if(canFade)
+            lerpAlpha();
+        
+    }
     
     void ChangeCamera()
     {
@@ -39,5 +63,41 @@ public class CameraShift : MonoBehaviour {
             ortho.SetActive(true);
             persp.SetActive(false);
         }
+    }
+
+    void lerpAlpha()
+    {
+        // change alpha depending on time
+        float lerp = Mathf.PingPong(Time.unscaledTime - startTime, duration)/ duration;
+        s_fade.a = lerp;
+        sprite.color = s_fade;
+    }
+
+    IEnumerator CameraChange()
+    {
+        float timeToWait = duration;
+        while (timeToWait >= 0f)
+        {
+            timeToWait -= Time.unscaledDeltaTime;
+            yield return null;
+        }
+        ChangeCamera();
+    }
+
+    IEnumerator ShiftFade()
+    {
+        Time.timeScale = 0;
+        // stop changing alpha after duration*2
+        float timeToWait = duration + duration;
+        while (timeToWait >= 0f)
+        {
+            timeToWait -= Time.unscaledDeltaTime;
+            yield return null;
+            //yield return new WaitForSeconds(duration + duration);
+        }
+        s_fade.a = 0;
+        sprite.color = s_fade;
+        canFade = false;    
+        Time.timeScale = 1;
     }
 }
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+    // should be different 3D 2D
+    void Wander()
+    {
+        // 2D back and forth
+
+        // 3D wander
+    }
+
+    void Seek()
+    {
+
+    }
+}
