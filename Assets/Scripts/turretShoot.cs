0bcb38584b618ff0eea9a408bc7d8eeb7cc1b255
Wade Gushikuma <wade.gushikuma@students.aie.edu.au>
2016-03-16 17:43:01 -0700

    fixed camera shift to work with player rotating

---
+++ Assets/Scripts/turretShoot.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turretShoot : MonoBehaviour {
+
+    private Animator anim;
+
+    public bool inRange;
+
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (inRange)
+        {
+            anim.SetBool("canShoot", true);
+        }
+        else
+            anim.SetBool("canShoot", false);
+	}
+
+    void OnTriggerEnter()
+    {
+        inRange = true;
+    }
+
+    void OnTriggerExit()
+    {
+        inRange = false;
+    }
+}
