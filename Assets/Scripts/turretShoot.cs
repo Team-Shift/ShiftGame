6e8b71b28eb66c9f732f207de5d15e30afdb0566
Wade Gushikuma <wade.gushikuma@students.aie.edu.au>
2016-03-17 16:57:16 -0700

    try again... turret working

--- Assets/Scripts/turretShoot.cs
+++ Assets/Scripts/turretShoot.cs
@@ -3,33 +3,52 @@ using System.Collections;
 
 public class turretShoot : MonoBehaviour {
 
-    private Animator anim;
+    private Animator anim;          // get turret animator
+    public GameObject fireball;     // prefab
+    private GameObject objToFollow;
 
-    public bool inRange;
+    public bool inRange;           // when to fire
 
-	// Use this for initialization
 	void Start () {
         anim = gameObject.GetComponent<Animator>();
 	}
 	
-	// Update is called once per frame
-	void FixedUpdate ()
+	void Update ()
     {
         if (inRange)
         {
+            // play turret anim
             anim.SetBool("canShoot", true);
+
+            // rotate turret to follow
+            gameObject.transform.LookAt(objToFollow.transform);
         }
-        else
-            anim.SetBool("canShoot", false);
+        else anim.SetBool("canShoot", false);
 	}
 
-    void OnTriggerEnter()
+    void OnTriggerEnter(Collider other)
     {
-        inRange = true;
+        // only fire if collider is player
+        if (other.tag == "Player")
+        {
+            Debug.Log(other.name);
+            inRange = true;
+            objToFollow = GameObject.FindGameObjectWithTag("Player");
+        }
     }
 
     void OnTriggerExit()
     {
         inRange = false;
+    }
+
+    void ShootProjectile()
+    {
+        Vector3 pos = new Vector3(gameObject.transform.position.x , gameObject.transform.position.y + 0.5f, gameObject.transform.position.z);
+
+        Quaternion rot = gameObject.transform.rotation;
+        rot *= Quaternion.Euler(0, 90, 0); // rotating wierdly 
+        // instantiate fire
+        Instantiate(fireball, pos, rot);
     }
 }
