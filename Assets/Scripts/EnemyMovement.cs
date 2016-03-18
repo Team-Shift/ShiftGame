e8d652b7c0eb1337380243a985df8bf0ecac49d2
Wade Gushikuma <wade.gushikuma@students.aie.edu.au>
2016-03-18 12:56:15 -0700

    seek working

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -2,15 +2,30 @@
 using System.Collections;
 
 public class EnemyMovement : MonoBehaviour {
+    
+    public float maxVel = 50;
+    public bool shouldSeek;
 
-    public float speed;
+    private Vector2 targetPos;
+    private Rigidbody rb;
+    private Vector2 desiredVel;
+    private Vector2 vel;
+    private GameObject player;
 
-	void Start () {
-	
-	}
+
+    void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+        shouldSeek = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
 
 	void Update () {
-	
+        if (shouldSeek)
+        {
+            targetPos = new Vector2(player.transform.position.x, player.transform.position.z);
+            vel = new Vector3(rb.velocity.x, rb.velocity.z);
+            Seek();
+        }
 	}
 
     // should be different 3D 2D
@@ -23,13 +38,38 @@ public class EnemyMovement : MonoBehaviour {
 
     void Seek()
     {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Vector2 v = new Vector2(targetPos.x - gameObject.transform.position.x, targetPos.y - gameObject.transform.position.z);
+        desiredVel = v * maxVel * Time.deltaTime;
 
+        Vector2 steering = desiredVel - vel;
+
+        gameObject.transform.LookAt(new Vector3(targetPos.x, 0, targetPos.y), Vector3.up);
+
+        Debug.Log(vel);
+
+        steering = steering / rb.mass;
+
+        rb.velocity += new Vector3(steering.x, 0, steering.y);
+
+        
+
+        //gameObject.transform.position += rb.velocity;
+        
       ///  gameObject.transform.position = ;   
     }
 
-    void OnTriggerEnter()
+    void OnTriggerEnter(Collider other)
     {
-        Seek();
+        if (other.tag == "Player")
+            shouldSeek = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            shouldSeek = false;
+            rb.velocity = Vector3.zero;
+        }
     }
 }
