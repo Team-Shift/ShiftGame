3dd9c9f0f1694204307bb94bbd5e8cca4eb1c1ab
Wade Gushikuma <wade.gushikuma@students.aie.edu.au>
2016-03-18 15:12:44 -0700

    seek and wander working

---
+++ Assets/Scripts/Seek.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Seek : MonoBehaviour {
+    public float maxVel = 50;
+    public bool shouldSeek;
+
+    private Vector2 targetPos;
+    private Rigidbody rb;
+    private Vector2 desiredVel;
+    private Vector2 vel;
+    private GameObject player;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        shouldSeek = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+        maxVel = 50;
+    }
+
+    void Update()
+    {
+        if (shouldSeek)
+        {
+            targetPos = new Vector2(player.transform.position.x, player.transform.position.z);
+            vel = new Vector3(rb.velocity.x, rb.velocity.z);
+
+            Vector2 v = new Vector2(targetPos.x - gameObject.transform.position.x, targetPos.y - gameObject.transform.position.z);
+            desiredVel = v * maxVel * Time.deltaTime;
+
+            Vector2 steering = desiredVel - vel;
+
+            gameObject.transform.LookAt(new Vector3(targetPos.x, 0, targetPos.y), Vector3.up);
+
+            steering = steering / rb.mass;
+
+            rb.velocity += new Vector3(steering.x, 0, steering.y);
+        }
+        else
+            rb.velocity = Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/batMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class batMovement : MonoBehaviour {
+
+    private Wander w;
+    private Seek s;
+
+    // Use this for initialization
+    void Start () {
+        w = GetComponent<Wander>();
+        s = GetComponent<Seek>();
+    }
+	
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            s.shouldSeek = true;
+            w.shouldWander = false;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            s.shouldSeek = false;
+            w.shouldWander = true;
+        }
+    }
+}
