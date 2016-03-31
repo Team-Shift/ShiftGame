4440d3a0fd7f2e0ff3153fe987ad2987c7317555
Jared Ramey <rameyjared@gmail.com>
2016-03-31 12:44:46 -0700

    Pushing changes to prefabs

--- Assets/Jared WorkSpace/Jareds_Scripts/EnemyDamageScript.cs
+++ Assets/Jared WorkSpace/Jareds_Scripts/EnemyDamageScript.cs
@@ -7,6 +7,8 @@ public class EnemyDamageScript : MonoBehaviour
     public GameObject player;
     public float xOffset;
     public float zOffset;
+    [HideInInspector]
+    private float yOffset;
 
     void Start()
     {
@@ -16,9 +18,16 @@ public class EnemyDamageScript : MonoBehaviour
 
     void Update()
     {
+        Vector3 playerPos = player.transform.position;
+        Vector3 fireBallPos = gameObject.transform.position;
+
+        yOffset = fireBallPos.y * .84f;
+        Debug.Log(yOffset);
+
         if (playerScript.CameraSwitch == false)
         {
-            if ((player.transform.position.x <= transform.position.x + xOffset && player.transform.position.x >= transform.position.x - xOffset) && (player.transform.position.z <= transform.position.z + zOffset && player.transform.position.z >= transform.position.z - zOffset))
+            //if ((player.transform.position.x <= transform.position.x + xOffset && player.transform.position.x >= transform.position.x - xOffset) && (player.transform.position.z <= transform.position.z + zOffset && player.transform.position.z >= transform.position.z - zOffset))
+            if( (playerPos.x <= fireBallPos.x + xOffset && playerPos.x >= fireBallPos.x - xOffset) && (playerPos.z <= (fireBallPos.z + yOffset) + zOffset && playerPos.z >= (fireBallPos.z + yOffset) - zOffset) )
             {
                 Debug.Log("Collision via x/z axis!");
                 Destroy(gameObject);
