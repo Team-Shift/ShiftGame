9bc9ca830c3daf3782c8908f50224c2e5f409811
ai tran <ai tran>
2016-04-07 13:42:55 -0700

    Polish Hearts and Dirt Kickoff Particle

--- Assets/AiStuff/Scripts/HeartHealthUI.cs
+++ Assets/AiStuff/Scripts/HeartHealthUI.cs
@@ -9,22 +9,25 @@ public class HeartHealthUI : MonoBehaviour {
     public GUITexture HeartContainerTexture;
     private List<GUITexture> HeartFillList = new List<GUITexture>();
     private List<GUITexture> HeartContainerList = new List<GUITexture>();
-    public int AmountOfHearts;
-    private float YOffset = 0.92f;
-    private float XOffset = 0.14f;
+    public int Health;
+    int AmountOfHeartContainer;
+    public float XOffset = 0.14f;
+    public float YOffset = 0.92f;
+    private int LoopYOffset;
+    private bool Loop = false;
 
 
     // Use this for initialization
     void Start()
     {
-
-        SpawnHeart(AmountOfHearts);
-        SpawnHeartContainer(AmountOfHearts);
+        AmountOfHeartContainer = Health;
+        SpawnHeart(Health);
+        SpawnHeartContainer(AmountOfHeartContainer);
     }
 
     void SpawnHeart(int HeartAmount)
     {
-        for (int i = 0; i < AmountOfHearts; i++)
+        for (int i = 0; i < Health; i++)
         {
             HeartFillList.Add(((GUITexture)Instantiate(HeartFillTexture, new Vector2(i * XOffset + .07f, YOffset), Quaternion.identity)));
         }
@@ -32,25 +35,41 @@ public class HeartHealthUI : MonoBehaviour {
 
     void SpawnHeartContainer(int HeartAmount)
     {
-        for (int i = 0; i < AmountOfHearts; i++)
+
+        for (int i = 0; i < Health; i++)
         {
             HeartContainerList.Add(((GUITexture)Instantiate(HeartContainerTexture, new Vector2(i * XOffset + .07f, YOffset), Quaternion.identity)));
         }
     }
 
-    public void AddHeart()
-    {
 
-        AmountOfHearts++;
-        int i = HeartContainerList.Count;
-            HeartFillList.Add(((GUITexture)Instantiate(HeartFillTexture, new Vector2(i * XOffset + .07f, YOffset), Quaternion.identity)));
-            HeartContainerList.Add(((GUITexture)Instantiate(HeartContainerTexture, new Vector2(i * XOffset + .07f, YOffset), Quaternion.identity)));
+    public void AddHeartContainer()
+    {
+        AmountOfHeartContainer++;
+        int HeartContainerIndex = HeartContainerList.Count;
+            HeartContainerList.Add(((GUITexture)Instantiate(HeartContainerTexture, new Vector3(HeartContainerIndex * XOffset + .07f, YOffset, -1), Quaternion.identity)));
     }
 
-    public void RemoveHeart()
+    public void HealHeart()
     {
-        AmountOfHearts--;
+        if (HeartFillList.Count < HeartContainerList.Count) {
+            Health++;
+            int HeartFillIndex = HeartFillList.Count;
+            HeartFillList.Add(((GUITexture)Instantiate(HeartFillTexture, new Vector2(HeartFillIndex * XOffset + .07f, YOffset), Quaternion.identity)));
+        }
+    }
+
+    public void DamageHeart()
+    {
+        Health--;
         Destroy(HeartFillList[HeartFillList.Count - 1].gameObject);
+        HeartFillList.RemoveAt(HeartFillList.Count - 1);
+    }
+
+    public void RemoveHeartContainer()
+    {
+        AmountOfHeartContainer--;
+        Destroy(HeartContainerList[HeartContainerList.Count - 1].gameObject);
         HeartFillList.RemoveAt(HeartFillList.Count - 1);
     }
 
