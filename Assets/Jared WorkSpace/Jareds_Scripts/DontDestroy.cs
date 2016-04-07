6297d432cb3d7d786374e77799c710b0eff720c3
Jared Ramey <rameyjared@gmail.com>
2016-04-07 15:23:59 -0700

    Started adding some background music

---
+++ Assets/Jared WorkSpace/Jareds_Scripts/DontDestroy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontDestroy : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        if (gameObject.GetComponent<AudioSource>())
+        {
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+    }
+}
