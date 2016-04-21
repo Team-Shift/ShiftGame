0de08406d826ffb557b5ee804e88ab4c027d303d
Shad0wth3if <sean.folk@students.aie.edu.au>
2016-04-21 16:23:22 -0700

    intro framework and rooms

---
+++ Assets/SeanStuff/Intro.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Intro : MonoBehaviour {
+
+    public GameObject[] slides;
+    public Animator anim;
+
+    int currentSlide = 0;
+
+    void Start()
+    {
+        for(int i=0; i < slides.Length; i++)
+        {
+            slides[i].SetActive (false);
+        }
+        slides[currentSlide].SetActive(true);
+    }
+
+    public void ChangeSlide(bool direction)
+    {
+        anim.SetBool("FadeOut", true);
+        StartCoroutine (SlideWait(direction)); 
+    }
+
+    public IEnumerator SlideWait(bool direction)
+    {
+        yield return new WaitForSeconds (1);
+        slides[currentSlide].SetActive(false);
+        if (direction == true)
+        {
+            currentSlide += 1;
+            if (currentSlide == slides.Length)
+            {
+                currentSlide = 0;
+            }
+        }
+        else if (direction == false)
+        {
+            currentSlide -= 1;
+            if (currentSlide == -1)
+            {
+                currentSlide = slides.Length - 1;
+            }
+        }
+        slides[currentSlide].SetActive(true);
+        anim.SetBool("FadeOut", false);
+        yield return true;
+    }
+}
