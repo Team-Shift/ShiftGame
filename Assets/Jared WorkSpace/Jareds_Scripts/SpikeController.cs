1c7296eb0fe6077c95344fb73f486a39f0b907d8
Jared Ramey <rameyjared@gmail.com>
2016-04-22 10:12:06 -0700

    Spikes are working (no animation) and fixed dirtkickoff particle not destroying after being initialized

--- Assets/Jared WorkSpace/Jareds_Scripts/SpikeController.cs
+++ Assets/Jared WorkSpace/Jareds_Scripts/SpikeController.cs
@@ -3,16 +3,34 @@ using System.Collections;
 
 public class SpikeController : MonoBehaviour
 {
+    public Custom2DController player;
+    private float posOffset;
 
     // Use this for initialization
     void Start()
     {
-
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Custom2DController>();
+        posOffset = gameObject.GetComponent<BoxCollider>().size.x / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 spikePos = gameObject.transform.position;
+        Vector3 playerPos = player.gameObject.transform.position;
+
+        if (Mathf.Abs(playerPos.x - spikePos.x) < posOffset && Mathf.Abs(playerPos.z - spikePos.z) < posOffset && playerPos.y > spikePos.y)
+        {
+            if(player.CameraSwitch == false)
+            {
+                //Activate animation
+                player.DamageFallback(gameObject.transform.position);
+            }
+        }
     }
 }
