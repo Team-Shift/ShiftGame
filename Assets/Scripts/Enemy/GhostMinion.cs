68f01b3f7aa0c1f3939e876fbfaa1897cdd28804
Jared Ramey <rameyjared@gmail.com>
2016-04-28 17:59:07 -0700

    More boss work and new ghost minnion added that boss spawns

---
+++ Assets/Scripts/Enemy/GhostMinion.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostMinion : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            col.gameObject.GetComponent<PlayerCombat>().DamageFallback(gameObject.transform.position);
+        }
+    }
+}
