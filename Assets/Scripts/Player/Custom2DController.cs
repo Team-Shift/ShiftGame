a69f4946edfdf3926bf4f8bd2e191990437c490c
ai tran <ai tran>
2016-05-05 16:24:53 -0700

    Fixed strafe animation trigger

--- Assets/Scripts/Player/Custom2DController.cs
+++ Assets/Scripts/Player/Custom2DController.cs
@@ -186,12 +186,17 @@ public class Custom2DController : MonoBehaviour
         if(strafe > 0)
         {
             //anim.SetTrigger("LeftStrafe");
-            //anim.SetFloat("x", -1);
+            anim.SetFloat("x", -1);
         }
         else if(strafe < 0)
         {
             //anim.SetTrigger("RightStrafe");
-            //anim.SetFloat("x", 1);
+            anim.SetFloat("x", 1);
+        }
+
+        if(strafe == 0)
+        {
+            anim.SetFloat("x", 0);
         }
 
         if (forwardBack != 0)
--- Assets/_Extensions/_NpcExtend.cs
+++ Assets/_Extensions/_NpcExtend.cs
@@ -4,35 +4,6 @@ using UnityEngine.UI;
 
 public static class _NpcExtend {
 
-    public static void ShopkeeperSpeech(this Transform self, int NPCSpeechIndex, string texts, GUIText guiText)
-    {
-        switch (NPCSpeechIndex)
-        {
-            case 5:
-                texts = "Turns out the witch was just lonely.";
-                guiText.text = texts;
-                break;
-            case 4:
-                texts = "Wow I'm three dimentional!";
-                guiText.text = texts;
-                break;
-            case 3:
-                texts = "Is that what the color of my shoes look like?";
-                guiText.text = texts;
-                break;
-            case 2:
-                texts = "Thank you for saving us!";
-                guiText.text = texts;
-                break;
-            case 1:
-                texts = "....";
-                guiText.text = texts;
-                break;
-            default:
-                texts = "Hi there player";
-                guiText.text = texts;
-                break;
-        }
-    }
+
 
 }
