8865e1384c1d2d4b4738c8a7a76c08c9c5a23ac9
ai tran <ai tran>
2016-05-12 14:48:52 -0700

    Fade Stuff Started

--- Assets/Scripts/Managers/PortalManager.cs
+++ Assets/Scripts/Managers/PortalManager.cs
@@ -1,11 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PortalManager : MonoBehaviour {
 
+    public Image img;
+
+    void Start()
+    {
+        img.color = Color.Lerp(img.color, Color.black, Time.deltaTime * 10);
+    }
 
     void OnTriggerEnter()
     {
-        this.transform.LoadScene(2);
+        if (img.color.a < 0.5f)
+        {
+            this.transform.LoadScene(2);
+        }
     }
+
 }
