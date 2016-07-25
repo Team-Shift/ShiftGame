0ebc833bd3e46d34a3922cf3392c55468bfbafe0
Wade Gushikuma <wade.gushikuma@students.aie.edu.au>
2016-07-25 02:11:32 -0700

    added room gen menu and window

---
+++ Assets/Scripts/CreateRoom.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateRoom : MonoBehaviour {
+
+	public void createTiles(int xTiles, int yTiles)
+	{
+		for (int i = 0; i < xTiles; i++) 
+		{
+			for (int j = 0; j < yTiles; j++) 
+			{
+				Instantiate (Resources.Load("forestfloor_center"), new Vector3(xTiles,0,yTiles));
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Editor/RoomGenEditor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class RoomGenEditor:EditorWindow {
+
+	string myString = "Hello World";
+	bool shouldEdit;
+	bool myBool = true;
+	float myFloat = 1.23f;
+
+	int x,y;
+
+	// Add menu item named "My Window" to the Window menu
+	[MenuItem("Create/Make Room")]
+	public static void ShowWindow()
+	{
+		//Show existing window instance. If one doesn't exist, make one.
+		EditorWindow.GetWindow(typeof(RoomGenEditor));
+		CreateRoom cr;
+	}
+
+	void OnGUI()
+	{
+		
+		GUILayout.Label ("Tiles Settings", EditorStyles.boldLabel);
+		x = EditorGUILayout.IntField ("Num Y Tiles", x);
+		y = EditorGUILayout.IntField ("Num Y Tiles", y);
+
+		shouldEdit = EditorGUILayout.BeginToggleGroup ("Edit Room", shouldEdit);
+
+		//myBool = EditorGUILayout.Toggle ("Toggle", myBool);
+		//myFloat = EditorGUILayout.Slider ("Slider", myFloat, -3, 3);
+		EditorGUILayout.EndToggleGroup ();
+
+		if (GUILayout.Button ("Create")) {
+			Debug.Log ("it worked");
+		}
+	}
+}
+	
