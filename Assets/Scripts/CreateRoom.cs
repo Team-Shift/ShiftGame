a4f5413825b7eaed94ce77084c474f3286d37386
Wade Gushikuma <wade.gushikuma@students.aie.edu.au>
2016-08-01 17:33:27 -0700

    finished room building tool

--- Assets/Scripts/CreateRoom.cs
+++ Assets/Scripts/CreateRoom.cs
@@ -3,17 +3,36 @@ using System.Collections;
 
 public class CreateRoom : MonoBehaviour {
 
-	public void createTiles(string name, int xTiles, int yTiles)
+	public void createTiles(string name, int xTiles, int yTiles, bool[,]toggles)
 	{
-		GameObject newRoom = new GameObject ();
+		GameObject newRoom = GameObject.Find(name);
+		if(newRoom)
+		{
+			DestroyImmediate (newRoom);
+			Debug.Log ("destorying obj");
+		}
+		newRoom = new GameObject ();
 		newRoom.name = name;
 		for (int i = 0; i < xTiles; i++) 
 		{
 			for (int j = 0; j < yTiles; j++) 
 			{
-				GameObject temp = Object.Instantiate (Resources.Load ("forestfloor_center"), new Vector3 (i, 0, j), Quaternion.identity) as GameObject;
-				temp.transform.SetParent (newRoom.transform);
+				if (!toggles [i, j]) {
+					GameObject temp = Object.Instantiate (Resources.Load ("forestfloor_center"), new Vector3 (i, 0, j), Quaternion.identity) as GameObject;
+					temp.transform.SetParent (newRoom.transform);
+				}
 			}
 		}
+		GameObject north = Object.Instantiate (Resources.Load ("North_Hallway"), new Vector3 (Mathf.CeilToInt (xTiles / 2), 0, -2), Quaternion.identity) as GameObject;
+		GameObject south = Object.Instantiate (Resources.Load ("South_Hallway"), new Vector3 (Mathf.CeilToInt (xTiles / 2), 0, yTiles+2), Quaternion.identity) as GameObject;
+		GameObject west = Object.Instantiate (Resources.Load ("West_Hallway"), new Vector3 (-2, 0, Mathf.CeilToInt (yTiles / 2)), Quaternion.identity) as GameObject;
+		GameObject east = Object.Instantiate (Resources.Load ("East_Hallway"), new Vector3 (yTiles+2, 0, Mathf.CeilToInt (yTiles / 2)), Quaternion.identity) as GameObject;
+		north.transform.SetParent (newRoom.transform);
+		south.transform.localRotation= Quaternion.Euler (0, 180, 0);
+		south.transform.SetParent (newRoom.transform);
+		west.transform.localRotation= Quaternion.Euler (0, 90, 0);
+		west.transform.SetParent (newRoom.transform);
+		east.transform.SetParent (newRoom.transform);
+		east.transform.localRotation= Quaternion.Euler (0, -90, 0);
 	}
 }
--- Assets/Scripts/Editor/RoomGenEditor.cs
+++ Assets/Scripts/Editor/RoomGenEditor.cs
@@ -5,11 +5,16 @@ using NUnit.Framework;
 public class RoomGenEditor:EditorWindow {
 
 	string myString = "Hello World";
-	bool shouldEdit;
+	bool shouldEdit = false;
 
-	int x,y;
+	static int MAXVALUE = 25;
+	bool buttonState;
+	static bool [,]tiles= new bool[MAXVALUE, MAXVALUE];
+	int x ,y;
 	string roomName;
 	public static CreateRoom instance;
+
+	int selected = 2;
 
 	// Add menu item named "My Window" to the Window menu
 	[MenuItem("Create/Make Room")]
@@ -18,22 +23,46 @@ public class RoomGenEditor:EditorWindow {
 		//Show existing window instance. If one doesn't exist, make one.
 		EditorWindow.GetWindow(typeof(RoomGenEditor));
 		CreateRoom cr;
+		InitArray (tiles);
+	}
+
+	static void InitArray(bool [,] arr)
+	{
+		for (int j = 0; j < MAXVALUE; j++) {
+			for (int i = 0; i < MAXVALUE; i++) {
+				arr [j, i] = false;
+			}
+		}
 	}
 
 	void OnGUI()
 	{
 		GUILayout.Label ("Tiles Settings", EditorStyles.boldLabel);
 		roomName = EditorGUILayout.TextField ("Name of Room", roomName);
+
 		x = EditorGUILayout.IntField ("Num Y Tiles", x);
+		if (x > MAXVALUE)
+			x = MAXVALUE;
 		y = EditorGUILayout.IntField ("Num Y Tiles", y);
+		if (y > MAXVALUE)
+			y = MAXVALUE;
 
-		shouldEdit = EditorGUILayout.BeginToggleGroup ("Edit Room", shouldEdit);
+		GUILayout.Label ("Edit Room", EditorStyles.boldLabel);
+
 		// draw layout of room 
-		EditorGUILayout.EndToggleGroup ();
-
+		for (int j = 0; j < y; j++) {
+			GUILayout.BeginHorizontal ();
+			for (int i = 0; i < x; i++) {
+				// do shit here
+				tiles[j,i] = GUILayout.Toggle(tiles[j,i], "", "button", GUILayout.Width(20), GUILayout.Height(20));
+				//tiles[j,i] = ! tiles[j,i];
+			}
+			GUILayout.EndHorizontal ();
+		}
+			
 		if (GUILayout.Button ("Create")) {
 			instance = new CreateRoom();
-			instance.createTiles (roomName,x, y);
+			instance.createTiles (roomName,x, y, tiles);
 		}
 	}
 }
